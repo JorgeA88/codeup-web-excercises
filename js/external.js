84bfbfc550d2428197b71a2334fd213ce3056952
Your Name <jorge.e.amador0626@gmail.com>
2020-07-24 11:02:49 -0500

    pushing

---
+++ js/external.js
@@ -0,0 +1,42 @@
+"use strict";
+console.log("external works")
+alert("Welcome to my website")
+var name = prompt("What's your favorite color?");
+alert("Great blue is my favorite color to.")
+var response = confirm("blue")
+console.log(typeof color);
+console.log("length", name.length);
+var movie1 = prompt ("How many days would You like this movie for?")
+var movie2 = prompt ("How many days would you like to rent this movie for?")
+var movie3 = prompt ("How many Days would You like to rent this Movie for")
+var subtotal = movie1 + movie2 + movie3
+var total = alert ("it will cost" + ( subtotal * 3))
+
+var googleR = prompt ("How much is Google paying you per hour?")
+var amazonR = prompt ("How much are you making an Hour?")
+var facebookR = prompt ("How much are you getting payed an hour?")
+var ghours = prompt ("How many hours did you work?")
+var ahours = prompt ("How many hours did you work?")
+var fbhours = prompt ("How many Hours did you work?")
+var agoogle = parseInt(googleR * ghours)
+var bamazon = parseInt( amazonR * ahours )
+var cfacebook = parseInt( facebookR * fbhours)
+var total = alert ("Your total is" + ( googleR * ghours) + ( amazonR * ahours) + ( facebookR * fbhours))
+
+classSize = 19;
+classSize == classRoomlimit
+classSize++;
+classSize == classRoomlimit
+classSize <= classRoomlimit
+var classSchedule = M,T,W,T,F;
+classSize <= classRoomlimit && (false)
+
+var Jorge = true
+var Keila = false
+var Jcart = 1
+var Kcart = 3
+Jorge || (Jcart => 2)
+Keila || (Kcart => 2)
+Keila == true || (Kcart => 2)
+Kcart => 2
+Keila || (Kcart >= 2)
