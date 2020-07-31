281345a6b56f5c61b4074df10164b9b5121b39bb
Your Name <jorge.e.amador0626@gmail.com>
2020-07-31 09:55:14 -0500

    updated break and continue

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,6 +1,17 @@
-var numberToSkip = prompt()
+var numberToSkip;
+
+while (true) {
+    numberToSkip = parseInt(prompt("Please choose a number between 1 and 50"));
+    if (numberToSkip % 2 !== 0 && numberToSkip > 0 && numberToSkip < 51)
+        break;
+    else continue;
+
+    console.log(numberToSkip);
+};
+
 for(var x = 1 ; x<= 49 ; x += 2) {
     if (x == numberToSkip) {
+        console.log('Yikes! Skipped the number:' + numberToSkip);
         continue;
     }
     console.log(x);
