36ddd38b9116263abfc847c48ae80f026dc357a9
Your Name <jorge.e.amador0626@gmail.com>
2020-07-31 10:07:41 -0500

    finished loops excersise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -17,27 +17,3 @@ for(var x = 1 ; x<= 49 ; x += 2) {
     console.log(x);
 }
 
- var k=1;
-    while(k <= 16){
-        console.log(Math.pow(2,k))
-        k++;
-}
-
-
-
-
-var allCones = Math.floor(Math.random()*50)+50;
-
-do {var coust= Math.floor(Math.random()*4)+1;
-    if(coust > allCones){
-        console.log("Cannot sell you"+ coust +"cones I only have"+ allCones +"...")
-    continue;
-    }
-    console.log(coust +" cones sold..")
-    allCones = allCones - coust;
-    if(allCones === 0){
-        console.log("yay! I sold them all..")
-    }
-
-}
-while(allCones > 0)
---
+++ js/while.js
@@ -0,0 +1,24 @@
+var k=1;
+while(k <= 16){
+    console.log(Math.pow(2,k))
+    k++;
+}
+
+
+
+
+var allCones = Math.floor(Math.random()*50)+50;
+
+do {var coust= Math.floor(Math.random()*4)+1;
+    if(coust > allCones){
+        console.log("Cannot sell you"+ coust +"cones I only have"+ allCones +"...")
+        continue;
+    }
+    console.log(coust +" cones sold..")
+    allCones = allCones - coust;
+    if(allCones === 0){
+        console.log("yay! I sold them all..")
+    }
+
+}
+while(allCones > 0)
